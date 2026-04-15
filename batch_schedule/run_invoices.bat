e51514fe73d3603c20f0c1b4597100b16fb79b5d
Joseph Patrick Timcang <jpltimcang@1go.ph>
2026-04-15 11:37:19 +0800

    Pushing updcate on run invoices.

--- batch_schedule/run_invoices.bat
+++ batch_schedule/run_invoices.bat
@@ -1,13 +1,13 @@
 cd c:\
 cd %~dp0\..
 cls
-php artisan api:invoice --sales-office="710100" --company="BII Live" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="750300" --company="BII Live" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="710200" --company="BII Live" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="750200" --company="BII Live" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="780500" --company="BMI" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="780800" --company="BMI" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="780900" --company="BMI" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="790500" --company="BMI" --date-from="2026-03-24" --is-auto=1
-php artisan api:invoice --sales-office="800100" --company="BMI" --date-from="2026-03-24" --is-auto=1
+php artisan api:invoice --sales-office="710100" --company="BII Live" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="750300" --company="BII Live" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="710200" --company="BII Live" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="750200" --company="BII Live" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="780500" --company="BMI" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="780800" --company="BMI" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="780900" --company="BMI" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="790500" --company="BMI" --date-from="2026-04-15" --is-auto=1
+php artisan api:invoice --sales-office="800100" --company="BMI" --date-from="2026-04-15" --is-auto=1
 timeout /t 5 /nobreak
